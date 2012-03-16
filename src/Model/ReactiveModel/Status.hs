2669e3594b01e64f0c86f0d6a043bf1f69609456
Ivan Perez <ivan.perez@keera.es>
2012-03-16 18:39:09 +0000

    Removes unused import. Simplifies operation using TH-based reactive fields generator.

--- src/Model/ReactiveModel/Status.hs
+++ src/Model/ReactiveModel/Status.hs
@@ -2,7 +2,6 @@
 module Model.ReactiveModel.Status where
 
 -- External imports
-import Data.Maybe
 import qualified Hails.MVC.Model.ReactiveFields as RFs
 import Hails.MVC.Model.ReactiveFields 
          (fieldGetter, fieldSetter, preTrue)
@@ -17,22 +16,6 @@ import Model.ReactiveModel.ModelEvents
 -- a Model, and it triggers a ModelEvent
 type Field a = RFs.Field a Model ModelEvent
 
--- | Set the new status.
---
--- The status remains unmodified if a model invariant is violated.
-setStatus :: ReactiveModel -> Status -> ReactiveModel
-setStatus rm n
- -- Nothing has changed
- | getStatus rm == n = rm
-
- -- Ok
- | otherwise   = triggerEvent rm' ev
-  where rm'    = rm `onBasicModel` (\b -> b { status = n })
-        ev     = StatusChanged
-
-getStatus :: ReactiveModel -> Status
-getStatus = status . basicModel
-
--- | Basic settings
+reactiveField "Status"              [t|Status|]
 reactiveField "LastKnownBalance"    [t|Maybe Int|]
 reactiveField "LastKnownExpiration" [t|Maybe String|]
