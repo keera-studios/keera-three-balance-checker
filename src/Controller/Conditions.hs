daec9643b128f6290ec8b89052adaf78a1e3767c
Ivan Perez <ivan.perez@keera.es>
2012-03-16 21:34:36 +0000

    Code cleaning

--- src/Controller/Conditions.hs
+++ src/Controller/Conditions.hs
@@ -10,28 +10,15 @@ module Controller.Conditions where
 import CombinedEnvironment
 
 -- Internal libraries: specific conditions
--- import qualified Controller.Conditions.MenuEnabled                    as MenuEnabled
--- import qualified Controller.Conditions.MenuEnabledClick               as MenuEnabledClick
--- -- import qualified Controller.Conditions.PreferencesDetectionMethods    as PreferencesDetectionMethods
-import qualified Controller.Conditions.PopupMenu                      as PopupMenu
-import qualified Controller.Conditions.Quit                           as Quit
--- import qualified Controller.Conditions.UpdateCheck                    as UpdateCheck
 import qualified Controller.Conditions.BalanceCheck as BalanceCheck
--- import qualified Controller.Conditions.Status                         as Status
-import qualified Controller.Conditions.Website                        as Website
--- import qualified Controller.Conditions.Help                           as Help
+import qualified Controller.Conditions.PopupMenu    as PopupMenu
+import qualified Controller.Conditions.Quit         as Quit
+import qualified Controller.Conditions.Website      as Website
 
 installHandlers :: CEnv -> IO()
 installHandlers cenv = do
-  -- Status.installHandlers                      cenv
-  -- MenuEnabled.installHandlers                 cenv
-  -- MenuEnabledClick.installHandlers            cenv
-  -- PreferencesDetectionMethods.installHandlers cenv
-  PopupMenu.installHandlers                   cenv
-  Quit.installHandlers                        cenv
-  -- Window.installHandlers                      cenv
-  -- UpdateCheck.installHandlers                 cenv
-  Website.installHandlers                     cenv
-  -- Help.installHandlers                        cenv
-  BalanceCheck.installHandlers                        cenv
+  BalanceCheck.installHandlers cenv
+  PopupMenu.installHandlers    cenv
+  Quit.installHandlers         cenv
+  Website.installHandlers      cenv
 
