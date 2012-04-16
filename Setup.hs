455ac99fbc02e9439e1f1aed7dfe7957ad0d7628
Ivan Perez <ivan.perez@keera.es>
2012-04-16 16:50:20 +0100

    hails clean

--- Setup.hs
+++ Setup.hs
@@ -1,5 +1,4 @@
 #!/usr/bin/env runhaskell
-{-# LANGUAGE BangPatterns #-}
 
 import Distribution.Simple
 import Distribution.Simple.Setup
@@ -15,7 +14,9 @@ import System.Directory
 import System.FilePath.Windows
 
 main = let hooks = simpleUserHooks 
-       in defaultMainWithHooks hooks { confHook = hailsConfigure }
+       in defaultMainWithHooks hooks { confHook  = hailsConfigure
+                                     , cleanHook = hailsClean
+                                     }
 
 -- This is a non-portable version of hails. It generates the
 -- files inside the src dir.
@@ -24,3 +25,9 @@ hailsConfigure (gpd, info) flags = do
   runProgramInvocation verbosity $ simpleProgramInvocation "hails" ["--init", "--output-dir=src"]
   (confHook simpleUserHooks) (gpd, info) flags 
  where verbosity = fromFlag $ configVerbosity flags
+
+hailsClean :: PackageDescription -> () -> UserHooks -> CleanFlags -> IO ()
+hailsClean pd () hooks flags = do
+  runProgramInvocation verbosity $ simpleProgramInvocation "hails" ["--clean", "--output-dir=src"]
+  (cleanHook simpleUserHooks) pd () hooks flags
+ where verbosity = fromFlag $ cleanVerbosity flags
