ae42d6400d14c3f47449f726f4372187e957af8d
Ivan Perez <ivan.perez@keera.es>
2012-08-02 00:09:55 +0200

    Cleans the code, adds comments.

--- src/Controller/Conditions/PopupMenu.hs
+++ src/Controller/Conditions/PopupMenu.hs
@@ -1,5 +1,6 @@
--- {-# LANGUAGE CPP #-}
 -- | Shows the popup menu when the user right-clicks the icon
+--
+-- (View => View)
 module Controller.Conditions.PopupMenu where
 
 import Control.Monad
@@ -7,15 +8,15 @@ import Graphics.UI.Gtk
 
 import CombinedEnvironment
 
+-- | Installs the event listeners
 installHandlers :: CEnv -> IO()
 installHandlers cenv = void $ do
   icon <- trayIcon $ mainWindowBuilder $ view cenv
   icon `on` statusIconPopupMenu $ condition cenv
 
+-- | Shows the menu when the user clicks on the icon
 condition :: CEnv -> Maybe MouseButton -> TimeStamp -> IO()
 condition cenv m t = onViewAsync $ do
   menu <- mainMenu $ mainWindowBuilder $ view cenv
-  -- This should be some other way
-
   let m' = fmap (flip (,) t) m
   menuPopup menu m'
--- src/Controller/Conditions/Website.hs
+++ src/Controller/Conditions/Website.hs
@@ -8,13 +8,17 @@ import Graphics.UI.Gtk.Helpers.MessageDialog
 import CombinedEnvironment
 import System.Application
 
+-- | Installs the event handlers for the website and Three's Allowance checking
+-- website
 installHandlers :: CEnv -> IO()
 installHandlers cenv = do
   associateMenuURL cenv mainMenuKeeraWebsite 
-                          "http://www.keera.es/projects/keera-posture/"
+                          "https://github.com/ivanperez-keera/keera-three-balance-checker"
   associateMenuURL cenv mainMenuThreeWebsite
                           "https://www.three.co.uk/My3Account/MBB_PAYG/Allowance"
 
+-- | Installs an event handler on a menu item that opens a website when
+-- activated
 associateMenuURL :: CEnv -> (Builder -> IO MenuItem) -> String -> IO()
 associateMenuURL cenv f url = void $ do
   menu <- f $ mainWindowBuilder $ view cenv
@@ -22,6 +26,7 @@ associateMenuURL cenv f url = void $ do
     onViewAsync $
        openUrlBySystemTool url >>= (`unless` reportNoBrowser)
 
+-- | Reports that no browser has been found
 reportNoBrowser :: IO ()
 reportNoBrowser =
   popupError "Keera PayG Balance Checker" 
